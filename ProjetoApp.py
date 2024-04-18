2bb25129f70130408c20c4f2a515ec7dfbcf4455
OctavioMoa <octavioaugusto098@gmail.com>
2024-04-18 01:34:00 -0300

    Correção App

--- ProjetoApp.py
+++ ProjetoApp.py
@@ -1,41 +1,42 @@
 # executar no terminal: pip install tabulate
 
 from tabulate import tabulate
-import time
+import time # Biblioteca ainda não usada
 
-CP = 36.0 # CUSTO PRODUTO
-CF = 15.0 # CUSTO FIXO / ADMINISTRATIVO
-CV = 5.0 # COMISSÃO DE VENDAS
-IV = 12.0 # IMPOSTO SOBRE VENDA
-ML = 20.0 # MARGEM DE LUCRO
+CP = float(input("Digite o custo do produto: ")) # CUSTO PRODUTO
+CF = float(input("Digite o custo fixo do produto: ")) # CUSTO FIXO
+CV = float(input("Digite a comissão de vendas do produto: ")) # COMISSÃO DE VENDAS
+IV = float(input("Digite os impostos do produto: ")) # IMPOSTOS SOBRE PRODUTO
+ML = float(input("Digite a margem de lucro do produto: ")) # MARGEM DE LUCRO
 PV = CP / (1 - (CF + CV + IV + ML) / 100) # PREÇO DE VENDA
 RB = PV-CP # RECEITA BRUTA
 OC = PV*(CF+CV+IV)/100 # OUTROS CUSTOS (CF + CV + IV)
 RT = RB-OC # RENTABILIDADE
 
 tabelaResultados = [["Descrição","Valor","%"],
-          ["Preço de Venda",PV,"100.0%"],
-          ["Custo de aquisição",CP,f"{(CP/PV)*100}"+"%"],
-          ["Receita Bruta",PV-CP,f"{((PV-CP)/PV)*100}"+"%"],
-          ["Custo Fixo/Administrativo",(CF*PV)/100,f"{CF}"+"%"],
-          ["Comissão de Vendas",(CV*PV)/100,f"{CV}"+"%"],
-          ["Impostos",(IV*PV)/100,f"{IV}"+"%"],
-          ["Outros Custos",OC,f"{(OC/PV)*100}"+"%"],
-          ["Rentabilidade",RT,f"{(RT/PV)*100}"+"%"]]
+          ["A. Preço de Venda",f"R${round(PV,2)}","100.0%"],
+          ["B. Custo de aquisição",f"R${round(CP,2)}",f"{round(CP/PV*100,3)}"+"%"],
+          ["C. Receita Bruta",f"R${round(PV-CP,2)}",f"{round((PV-CP)/PV*100,3)}"+"%"],
+          ["D. Custo Fixo/Administrativo",f"R${round(CF*PV/100,2)}",f"{round(CF,3)}"+"%"],
+          ["E. Comissão de Vendas",f"R${round(CV*PV/100,2)}",f"{round(CV,3)}"+"%"],
+          ["F. Impostos",f"R${round(IV*PV/100,2)}",f"{round(IV,3)}"+"%"],
+          ["G. Outros Custos",f"R${round(OC,2)}",f"{round(OC/PV*100,3)}"+"%"],
+          ["H. Rentabilidade",f"R${round(RT,2)}",f"{round(RT/PV*100,3)}"+"%"]]
 
 print("\n\n TABELA DE RESULTADOS: \n")
 print(tabulate(tabelaResultados))
 print("\n\n CLASSIFICAÇÃO DE RENTABILIDADE: \n")
 if ((RT/PV)*100)>20:
-    print(f"Seu produto possui uma rentabilidade alta, a porcentagem de lucro será de {((RT/PV)*100)}"+"%")
-elif ((RT/PV)*100)<=20>10:
-    print(f"Seu produto possui uma rentabilidade média, a porcentagem de lucro será de {((RT/PV)*100)}"+"%")
-elif ((RT/PV)*100)<=10>0:
-    print(f"Seu produto possui uma rentabilidade baixa, a porcentagem de lucro será de {((RT/PV)*100)}"+"%")
-elif ((RT/PV)*100)==0:
+    print(f"Seu produto possui uma rentabilidade alta, a porcentagem de lucro será de {(round(RT/PV,2)*100)}"+"%")
+elif ((RT/PV)*100)<20 and ((RT/PV)*100)>10 or ((RT/PV)*100)==20:
+    print(f"Seu produto possui uma rentabilidade média, a porcentagem de lucro será de {(round(RT/PV,2)*100)}"+"%")
+elif ((RT/PV)*100)<10 and ((RT/PV)*100)>0 and ((RT/PV)*100)!=0 and ML!=0:
+    print(f"Seu produto possui uma rentabilidade baixa, a porcentagem de lucro será de {(round(RT/PV,2)*100)}"+"%")
+elif ((RT/PV)*100)==0 or ML==0:
     print(f"Seu produto não irá gerar lucro")
-elif ((RT/PV)*100)==0:
-    print(f"Seu produto acarretará em prejuízos de {((RT/PV)*100)}"+"%")
+elif ((RT/PV)*100)<0:
+    print(f"Seu produto acarretará em prejuízos de {(round(RT/PV,2)*100)}"+"%")
 else:
     print("ERRO, TENTE NOVAMENTE!")
 print()
+print("O programa irá encerrar em 10 segundos.")
